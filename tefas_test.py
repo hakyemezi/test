9d3ae28e0ea94ab762a814b4fda53e39cb5aff52
hakyemezi <128547149+hakyemezi@users.noreply.github.com>
2023-08-04 23:41:35 +0300

    tefas_test.py

--- tefas_test.py
+++ tefas_test.py
@@ -3,6 +3,7 @@ import requests
 import pandas as pd
 from datetime import datetime
 
+
 def isportfoy():
     is_portfoy_fund_list = ['IAG', 'IYG', 'ISZ']
     url = 'https://www.isportfoy.com.tr/tr/yatirim-fonlari'
@@ -34,8 +35,29 @@ def fund_price(fund_code, start_date, end_date, type="YAT"):
     else:
         return None
 
+def akportfoy():
+    url_list = ["https://www.akportfoy.com.tr/tr/fon/BYG-ak-portfoy-yonetimi-a.s.-birinci-yenilenebilir-enerji-girisim-sermayesi-yatirim-fonu",
+                "https://www.akportfoy.com.tr/tr/fon/BGY-ak-portfoy-yonetimi-a.s.-birinci-gayrimenkul-yatirim-fonu",
+                "https://www.akportfoy.com.tr/tr/fon/IKG-akportfoy-yonetimi-a.s.-ikinci-gayrimenkul-yatirim-fonu",
+                "https://www.akportfoy.com.tr/tr/fon/AGN-ak-portfoy-yonetimi-a.s.-birinci-girisim-sermayesi-yatirim-fonu",
+                "https://www.akportfoy.com.tr/tr/fon/ICI-ak-portfoy-yonetimi-a.s.-ikinci-girisim-sermayesi-yatirim-fonu",]
+
+    akportfoy = pd.DataFrame(columns=["FUND", "PRICE"])
+
+    for url in url_list:
+        parts = url.split('-')
+        fund_code = parts[0].split('/')[-1]
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        info_card_value = soup.find(class_="info-card-value info-card-value-group")
+        fund_price = info_card_value.get_text(strip=True)
+        akportfoy_temp = pd.DataFrame({"FUND": [fund_code],
+                                       "PRICE": [fund_price]})
+        akportfoy = pd.concat([akportfoy, akportfoy_temp], ignore_index=True)
+
+    return akportfoy
+
 def main():
-   
     if st.button("Start"):
         current_date = datetime.now().date()
         formatted_date_1 = current_date.strftime("%d/%m/%Y")
@@ -54,8 +76,9 @@ def main():
         kontrol = kontrol[["FONKODU", "FIYAT"]]
         kontrol.columns = ['FUND', 'PRICE']
 
-        new_df = isportfoy()
-        result = pd.concat([kontrol, new_df]).reset_index(drop=True)
+        is_portfoy = isportfoy()
+        ak_portfoy = akportfoy()
+        result = pd.concat([kontrol, is_portfoy, ak_portfoy]).reset_index(drop=True)
 
         export = pd.DataFrame({"date": formatted_date_1,
                                "fund": result["FUND"],
@@ -68,13 +91,14 @@ def main():
         export.to_csv(f'{export_filename}', index=False, header=False)
 
         st.success("Ready.")
-       
+
         st.download_button(
             label="Download",
             data=export.to_csv(index=False, header=False, sep=";").encode('utf-8'),
             file_name=export_filename,
             mime='text/csv',
         )
+
 
 if __name__ == "__main__":
     main()
