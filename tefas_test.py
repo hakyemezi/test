7ac3129cc7007945e30542cacd4d2dc8e1ccf055
hakyemezi <128547149+hakyemezi@users.noreply.github.com>
2023-08-04 17:25:55 +0300

    tefas_test.py

--- tefas_test.py
+++ tefas_test.py
@@ -3,6 +3,38 @@ import requests
 import pandas as pd
 from datetime import datetime
 
+
+def isportfoy():
+    is_portfoy_fund_list = ['IAG', 'IYG', 'ISZ']
+    url = 'https://www.isportfoy.com.tr/tr/yatirim-fonlari'
+    html = requests.get(url).content
+    df_list = pd.read_html(html, encoding='utf-8', decimal=',', thousands='.')
+    df = df_list[0]
+    new_df = df[df[('Fon Kodu', 'Fon Kodu')].isin(is_portfoy_fund_list)]
+    new_df = new_df[[('Fon Kodu', 'Fon Kodu'), ('Birim Pay Fiyatı', 'Birim Pay Fiyatı')]]
+    new_df.columns = ['FUND', 'PRICE']
+    return new_df
+
+
+def fund_price(fund_code, start_date, end_date, type="YAT"):
+    url = "https://www.tefas.gov.tr/api/DB/BindHistoryInfo"
+    data = {
+        "fontip": type,
+        "sfontur": "",
+        "fonkod": fund_code,
+        "fongrup": "",
+        "bastarih": start_date,
+        "bittarih": end_date,
+        "fonturkod": "",
+        "fonunvantip": "",
+    }
+    response = requests.post(url, data=data)
+    if response.status_code == 200:
+        dataframe = pd.DataFrame(response.json()["data"])
+        return dataframe
+    else:
+        return None
+
 def main():
     st.title("Fon Fiyatları Çekme Uygulaması")
 
@@ -12,25 +44,6 @@ def main():
         formatted_date_1 = current_date.strftime("%d/%m/%Y")
         formatted_date_2 = current_date.strftime("%d.%m.%Y")
         formatted_date_3 = current_date.strftime("%Y%m%d")
-
-        def fund_price(fund_code, start_date, end_date, type="YAT"):
-            url = "https://www.tefas.gov.tr/api/DB/BindHistoryInfo"
-            data = {
-                "fontip": type,
-                "sfontur": "",
-                "fonkod": fund_code,
-                "fongrup": "",
-                "bastarih": start_date,
-                "bittarih": end_date,
-                "fonturkod": "",
-                "fonunvantip": "",
-            }
-            response = requests.post(url, data=data)
-            if response.status_code == 200:
-                dataframe = pd.DataFrame(response.json()["data"])
-                return dataframe
-            else:
-                return None
 
         fund_list = ['GPA', 'TPL', 'IJT', 'TGE', 'AFA', 'AFS', 'AFT', 'KLU', 'TCA',
                      'TGE', 'AES', 'TPC', 'DLY', 'DCB', 'DTL', 'DBH', 'TUA', 'DPK',
@@ -44,17 +57,6 @@ def main():
 
         kontrol = kontrol[["FONKODU", "FIYAT"]]
         kontrol.columns = ['FUND', 'PRICE']
-
-        def isportfoy():
-            is_portfoy_fund_list = ['IAG', 'IYG', 'ISZ']
-            url = 'https://www.isportfoy.com.tr/tr/yatirim-fonlari'
-            html = requests.get(url).content
-            df_list = pd.read_html(html, encoding='utf-8', decimal=',', thousands='.')
-            df = df_list[0]
-            new_df = df[df[('Fon Kodu', 'Fon Kodu')].isin(is_portfoy_fund_list)]
-            new_df = new_df[[('Fon Kodu', 'Fon Kodu'), ('Birim Pay Fiyatı', 'Birim Pay Fiyatı')]]
-            new_df.columns = ['FUND', 'PRICE']
-            return new_df
 
         new_df = isportfoy()
         result = pd.concat([kontrol, new_df]).reset_index(drop=True)
