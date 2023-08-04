7bc38f477594cd255d96f1a542a043a45ee83d60
hakyemezi <128547149+hakyemezi@users.noreply.github.com>
2023-08-04 17:09:28 +0300

    Add files via upload

---
+++ tefas_test.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import requests
+import pandas as pd
+from datetime import datetime
+
+def main():
+    st.title("Fon Fiyatları Çekme Uygulaması")
+
+    if st.button("Fon Fiyatlarını Çek"):
+        current_date = datetime.now().date()
+
+        formatted_date_1 = current_date.strftime("%d/%m/%Y")
+        formatted_date_2 = current_date.strftime("%d.%m.%Y")
+        formatted_date_3 = current_date.strftime("%Y%m%d")
+
+        def fund_price(fund_code, start_date, end_date, type="YAT"):
+            url = "https://www.tefas.gov.tr/api/DB/BindHistoryInfo"
+            data = {
+                "fontip": type,
+                "sfontur": "",
+                "fonkod": fund_code,
+                "fongrup": "",
+                "bastarih": start_date,
+                "bittarih": end_date,
+                "fonturkod": "",
+                "fonunvantip": "",
+            }
+            response = requests.post(url, data=data)
+            if response.status_code == 200:
+                dataframe = pd.DataFrame(response.json()["data"])
+                return dataframe
+            else:
+                return None
+
+        fund_list = ['GPA', 'TPL', 'IJT', 'TGE', 'AFA', 'AFS', 'AFT', 'KLU', 'TCA',
+                     'TGE', 'AES', 'TPC', 'DLY', 'DCB', 'DTL', 'DBH', 'TUA', 'DPK',
+                     'TCF', 'TJI', 'TRR', 'PUR', 'FSK', 'PPZ', 'NRG', 'TE4', 'TE3',
+                     'DYN', 'DVT', 'DLD']
+
+        kontrol = pd.DataFrame()
+        for fund in fund_list:
+            price = fund_price(fund, formatted_date_2, formatted_date_2)
+            kontrol = pd.concat([kontrol, price])
+
+        kontrol = kontrol[["FONKODU", "FIYAT"]]
+        kontrol.columns = ['FUND', 'PRICE']
+
+        def isportfoy():
+            is_portfoy_fund_list = ['IAG', 'IYG', 'ISZ']
+            url = 'https://www.isportfoy.com.tr/tr/yatirim-fonlari'
+            html = requests.get(url).content
+            df_list = pd.read_html(html, encoding='utf-8', decimal=',', thousands='.')
+            df = df_list[0]
+            new_df = df[df[('Fon Kodu', 'Fon Kodu')].isin(is_portfoy_fund_list)]
+            new_df = new_df[[('Fon Kodu', 'Fon Kodu'), ('Birim Pay Fiyatı', 'Birim Pay Fiyatı')]]
+            new_df.columns = ['FUND', 'PRICE']
+            return new_df
+
+        new_df = isportfoy()
+        result = pd.concat([kontrol, new_df]).reset_index(drop=True)
+
+        export = pd.DataFrame({"date": formatted_date_1,
+                               "fund": result["FUND"],
+                               "price1": result["PRICE"],
+                               "price2": result["PRICE"],
+                               "price3": result["PRICE"]})
+
+        export_filename = f'{formatted_date_3}.csv'
+
+        export.to_csv(f'{export_filename}', index=False, header=False)
+
+        st.success("Fon fiyatları başarıyla çekildi")
+
+        st.download_button(
+            label="Dosyayı İndir",
+            data=export.to_csv(index=False, header=False, sep=";").encode('utf-8'),
+            file_name=export_filename,
+            mime='text/csv',
+        )
+
+if __name__ == "__main__":
+    main()
